6477be34a7209376da76e7c5a4eec4d47c08173b
Niko Gonzalez <nikogonzalez@nikos-mbp.lan>
2018-08-01 22:16:13 -0700

    update to check adgroups, seems to work so far

--- Pause On Date Script.js
+++ Pause On Date Script.js
@@ -1,47 +1,45 @@
-//https://searchenginewatch.com/sew/how-to/2417479/adwords-scripts-discovery-pause-and-enable-ads-based-on-dates
-
-var ENTITY = 'Ad'; //or Ad or Campaign
-var PAUSE_PREFIX = "Pause on "; //look for labels "Pause on 2013-04-11"
-var ENABLE_PREFIX = "Enable on "; //look for labels "Enable on 2013-04-11"
+//sets adgroup as the entity in use
+var ENTITY = 'AdGroup';
+//adds 'pause on' string
+var PAUSE_PREFIX = "Pause on ";
+//adds 'enable on' string
+var ENABLE_PREFIX = "Enable on ";
 
 
 function main() {
   //get current day from AdWords app
-  var todayStr = Utilities.formatDate(new Date(), AdWordsApp.currentAccount().getTimeZone(), "yyyy-MM-dd");
-  var pauseStr = PAUSE_PREFIX+todayStr;
-  var enableStr = ENABLE_PREFIX+todayStr;
-  Logger.log("Looking for labels: " + [pauseStr,enableStr].join(' and '));
+  var today = Utilities.formatDate(new Date(), AdWordsApp.currentAccount().getTimeZone(), "yyyy-MM-dd");
+  //sets variable to 'Pause on yyyy-MM-dd
+  var pauseAdGroup = PAUSE_PREFIX+today;
+  //sets variable to 'Enable on yyyy-MM-dd
+  var enableAdGroup = ENABLE_PREFIX+today;
+  Logger.log("Looking for labels: " + [pauseAdGroup,enableAdGroup].join(' and '));
 
-  var labelsArray = buildLabelArray(pauseStr,enableStr);
+  var labelArray = buildLabelArray(pauseAdGroup,enableAdGroup);
 
-  if(labelsArray.length > 0) {
-    var labelsStr = "['" + labelsArray.join("','") + "']";
-    var entityIter;
-    if(ENTITY === 'Keyword') {
-      entityIter = AdWordsApp.keywords().withCondition("LabelNames CONTAINS_ANY "+labelsStr).get();
-    } else if(ENTITY === 'Ad') {
-      entityIter = AdWordsApp.ads().withCondition("LabelNames CONTAINS_ANY "+labelsStr).get();
-    } else if(ENTITY === 'Campaign') {
-      entityIter = AdWordsApp.campaigns().withCondition("LabelNames CONTAINS_ANY "+labelsStr).get();
-    } else {
-      throw 'Invalid ENTITY type. ENTITY should be Campaign, Keyword or Ad. ENTITY:'+ENTITY;
-    }
+  if(labelArray.length > 0) {
+   var labelizer = "['" + labelArray.join("','") + "']";
+   var entityIterator;
+   if (ENTITY === 'AdGroup') {
+       entityIterator = AdWordsApp.adGroups().withCondition("LabelNames CONTAINS_ANY "+labelizer).get();
+       } else {
+       throw "invalid entity yo";
+       }
 
-    while(entityIter.hasNext()) {
-      var entity = entityIter.next();
-      pauseEntity(entity, pauseStr);
-      enableEntity(entity, enableStr);
-    }
+       while(entityIterator.hasNext()) {
+       	var entity = entityIterator.next();
+        pauseEntity(entity, pauseAdGroup);
+        enableEntity(entity, enableAdGroup);
+       }
   }
-}
 
-//Helper function to build a list of labels in the account
-function buildLabelArray(pauseStr,enableStr) {
+  //Helper function to build a list of labels in the account
+function buildLabelArray(pauseAdGroup,enableAdGroup) {
   var labelsArray = [];
   try {
-    var labelIter = AdWordsApp.labels().withCondition("Name IN ['"+pauseStr+"','"+enableStr+"']").get();
-    while(labelIter.hasNext()) {
-      labelsArray.push(labelIter.next().getName());
+    var labelIterator = AdWordsApp.labels().withCondition("Name IN ['"+pauseAdGroup+"','"+enableAdGroup+"']").get();
+    while(labelIterator.hasNext()) {
+      labelsArray.push(labelIterator.next().getName());
     }
     return labelsArray;
   } catch(e) {
@@ -52,18 +50,21 @@ function buildLabelArray(pauseStr,enableStr) {
 
 //Helper function to pause entities
 function pauseEntity(entity, pauseStr) {
-  var labelIter = entity.labels().withCondition("Name = '"+pauseStr+"'").get();
-  if(labelIter.hasNext()) {
+  var labelIterator = entity.labels().withCondition("Name = '"+pauseAdGroup+"'").get();
+  if(labelIterator.hasNext()) {
     entity.pause();
-    entity.removeLabel(pauseStr);
+    entity.removeLabel(pauseAdGroup);
   }
 }
 
 //Helper function to enable entities
 function enableEntity(entity, enableStr) {
-  var labelIter = entity.labels().withCondition("Name = '"+enableStr+"'").get();
-  if(labelIter.hasNext()) {
+  var labelIterator = entity.labels().withCondition("Name = '"+enableAdGroup+"'").get();
+  if(labelIterator.hasNext()) {
     entity.enable();
-    entity.removeLabel(enableStr);
+    entity.removeLabel(enableAdGroup);
   }
+
+
+}
 }
